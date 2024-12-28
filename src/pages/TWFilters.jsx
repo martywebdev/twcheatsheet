9d74e54cda50275edb905fd7296e2e0b3e42742b
Marty Umlas <martyumlas@gmail.com>
2024-12-28 12:31:55 +0800

    initial transformation

--- src/pages/TWFilters.jsx
+++ src/pages/TWFilters.jsx
@@ -55,40 +55,46 @@ const TWFilters = () => {
         </p>
       </div>
 
-      <div className="flex gap-2">
-        <Select
-          isDark
-          options={backdropBrightnessList}
-          handleChange={setBackdrop}
-          label={"brightness"}
-        />
+      <div>
+        <div className="flex gap-2">
+          <Select
+            isDark
+            options={backdropBrightnessList}
+            handleChange={setBackdrop}
+            label={"brightness"}
+          />
 
-        <Select
-          isDark
-          options={backdropBlurs}
-          handleChange={setBackdropBlur}
-          label={"blur"}
-        />
-      </div>
-      <div
-        className={`bg-[url('/img/backdrop2.jpg')] min-h-screen bg-cover bg-center flex justify-center items-center `}
-      >
-        <div
-          className={`rounded-xl border bg-white/60 p-5 w-5/6 ${backdrop} ${backdropBlur}`}
-        >
-          <h3 className="text-3xl font-semibold mb-2">Backdrop</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-            quisquam neque similique tempora est! Molestiae dolorum eius,
-            eveniet possimus laudantium deserunt ratione nostrum, cupiditate
-            natus eligendi illum? Soluta, quam deleniti.
-          </p>
+          <Select
+            isDark
+            options={backdropBlurs}
+            handleChange={setBackdropBlur}
+            label={"blur"}
+          />
         </div>
+        <div
+          className={`bg-[url('/img/backdrop2.jpg')] min-h-screen bg-cover bg-center flex justify-center items-center `}
+        >
+          <div
+            className={`rounded-xl border bg-white/60 p-5 w-5/6 ${backdrop} ${backdropBlur}`}
+          >
+            <h3 className="text-3xl font-semibold mb-2">Backdrop</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              quisquam neque similique tempora est! Molestiae dolorum eius,
+              eveniet possimus laudantium deserunt ratione nostrum, cupiditate
+              natus eligendi illum? Soluta, quam deleniti.
+            </p>
+          </div>
+        </div>
+        <p className="bg-black text-white p-4 hover:invert border-2">
+          check also use other options here! like backdrop-sepia
+          backdrop-grayscale
+        </p>
       </div>
-      <p className="bg-black text-white p-4 hover:invert border-2">
-        check also use other options here! like backdrop-sepia
-        backdrop-grayscale
-      </p>
+
+      <div className="container max-w-screen-sm h-auto mx-auto">
+        <img src="/img/cyberpunk-small.jpg" alt="" className="h-[300px]" />
+      </div>
     </>
   );
 };
