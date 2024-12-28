2b7526ce4b41338184ab8f18062b468499dc4994
Marty Umlas <martyumlas@gmail.com>
2024-12-28 12:08:13 +0800

    update filters data

---
+++ src/data/filters.js
@@ -0,0 +1,58 @@
+export const blurs = [
+  "blur-none",
+  "blur-sm",
+  "blur-md",
+  "blur-lg",
+  "blur-xl",
+  "blur-2xl",
+  "blur-3xl",
+];
+
+export const backdropBlurs = [
+  "backdrop-blur-none",
+  "backdrop-blur-sm",
+  "backdrop-blur-md",
+  "backdrop-blur-lg",
+  "backdrop-blur-xl",
+  "backdrop-blur-2xl",
+  "backdrop-blur-3xl",
+];
+
+export const brightnessList = [
+  "",
+  "brightness-0",
+  "brightness-50",
+  "brightness-70",
+  "brightness-90",
+  "brightness-95",
+  "brightness-100",
+  "brightness-105",
+  "brightness-125",
+  "brightness-150",
+  "brightness-200",
+];
+
+export const backdropBrightnessList = [
+  "",
+  "backdrop-brightness-0",
+  "backdrop-brightness-50",
+  "backdrop-brightness-70",
+  "backdrop-brightness-90",
+  "backdrop-brightness-95",
+  "backdrop-brightness-100",
+  "backdrop-brightness-105",
+  "backdrop-brightness-125",
+  "backdrop-brightness-150",
+  "backdrop-brightness-200",
+];
+export const contrastList = [
+  "contrast-more",
+  "contrast-less",
+  "contrast-0",
+  "contrast-50",
+  "contrast-75",
+  "contrast-100",
+  "contrast-125",
+  "contrast-150",
+  "contrast-200",
+];
--- src/pages/TWFilters.jsx
+++ src/pages/TWFilters.jsx
@@ -1,63 +1,7 @@
 import { useState } from "react";
 import Select from "../components/Select";
-const blurs = [
-  "blur-none",
-  "blur-sm",
-  "blur-md",
-  "blur-lg",
-  "blur-xl",
-  "blur-2xl",
-  "blur-3xl",
-];
+import { backdropBlurs, backdropBrightnessList, blurs, brightnessList, contrastList } from "../data/filters";
 
-const backdropBlurs = [
-  "backdrop-blur-none",
-  "backdrop-blur-sm",
-  "backdrop-blur-md",
-  "backdrop-blur-lg",
-  "backdrop-blur-xl",
-  "backdrop-blur-2xl",
-  "backdrop-blur-3xl",
-];
-
-const brightnessList = [
-  "",
-  "brightness-0",
-  "brightness-50",
-  "brightness-70",
-  "brightness-90",
-  "brightness-95",
-  "brightness-100",
-  "brightness-105",
-  "brightness-125",
-  "brightness-150",
-  "brightness-200",
-];
-
-const backdropBrightnessList = [
-  "",
-  "backdrop-brightness-0",
-  "backdrop-brightness-50",
-  "backdrop-brightness-70",
-  "backdrop-brightness-90",
-  "backdrop-brightness-95",
-  "backdrop-brightness-100",
-  "backdrop-brightness-105",
-  "backdrop-brightness-125",
-  "backdrop-brightness-150",
-  "backdrop-brightness-200",
-];
-const contrastList = [
-  "contrast-more",
-  "contrast-less",
-  "contrast-0",
-  "contrast-50",
-  "contrast-75",
-  "contrast-100",
-  "contrast-125",
-  "contrast-150",
-  "contrast-200",
-];
 
 const TWFilters = () => {
   const [blur, setBlur] = useState("");
